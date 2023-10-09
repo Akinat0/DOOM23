ace27f83e8e2a113ab5b813c084e373117983674
Студент BStudentD <student@obe.local>
2023-10-09 19:19:52 +0300

    wip

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,33 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float forwardForce = 1;
-    [SerializeField] float sideForce = 1;
-    [SerializeField] float sensitivity = 1;
+    [SerializeField] float speed = 10;
+    [SerializeField] float sensitivity = 10;
 
-    Rigidbody rb;
+    Vector3 surfaceNormal;
+
+    CharacterController characterController;
+    float verticalSpeed;
 
     private void Awake()
     {
-        rb = GetComponent<Rigidbody>();
+        characterController = GetComponent<CharacterController>();
     }
 
-    void FixedUpdate()
+    private void Update()
     {
-        float xMouseMovement = Input.GetAxis("Mouse X");
-        float horizontal = Input.GetAxis("Horizontal");           
-        float vertical = Input.GetAxis("Vertical");
+        Vector3 rotation = new Vector3(0, Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime);
+        transform.Rotate(rotation);
 
-        Vector3 force = Vector3.zero;
-        force += transform.forward * vertical * Time.fixedDeltaTime * forwardForce;
-        force += transform.right * horizontal * Time.fixedDeltaTime * sideForce;
+        if (characterController.isGrounded) 
+            verticalSpeed = -0.1f;
+        else
+            verticalSpeed += Physics.gravity.y * Time.deltaTime;
+        
 
-        float rotation = xMouseMovement * sensitivity * Time.fixedDeltaTime;
+        Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        input = Vector3.ClampMagnitude(input, 1);
 
-        rb.AddForce(force);
-        transform.Rotate(0, rotation, 0);
+        Vector3 velocity = transform.TransformDirection(input) * speed;
+
+        Quaternion slopeRotation = Quaternion.FromToRotation(Vector3.up, surfaceNormal);
+        Vector3 adjustedVelocity = slopeRotation * velocity;
+
+        velocity = adjustedVelocity.y < 0 ? adjustedVelocity : velocity;
+
+        velocity.y += verticalSpeed;
+
+        characterController.Move(velocity * Time.deltaTime);
+    }
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Debug.DrawLine(hit.point, hit.point + hit.normal * 10, Color.red);
+
+        surfaceNormal = hit.normal;
     }
 }
